134f32ce1b4e41b92703157d0ba369cc3dcd2090
vader9595 <102444679+vader9595@users.noreply.github.com>
2022-08-02 16:03:08 -0500

    Initial commit

---
+++ src/main/java/com/example/SpringBoot_303/CourseRepository.java
@@ -0,0 +1,7 @@
+package com.example.SpringBoot_303;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Long> {
+
+        }
---
+++ src/test/java/com/example/SpringBoot_303/SpringBoot303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot303ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
